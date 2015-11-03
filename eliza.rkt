1dec661d1991abf6999174af7ffd878fa4818114
Gerardo Tarragona <geratarra@gmail.com>
2015-11-03 11:50:09 -0700

    Avanzando..

--- eliza.rkt
+++ eliza.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (provide pat-match)
+(provide position)
 
 (define fail 'fail)
 (define no-bindings '((#t . #t)))
@@ -20,11 +21,37 @@
         ((variable-p pattern)
          (match-variable pattern input bindings))
         ((eqv? pattern input) bindings)
+        ((segment-pattern? pattern)
+         (segment-match pattern input bindings))
         ((and (cons? pattern) (cons? input))
          (pat-match (cdr pattern) (cdr input)
                     (pat-match (car pattern) (car input)
                                 bindings)))
         (#t fail)))
+
+(define (segment-match pattern input bindings (start 0))
+  (let ((var (second (firts pattern)))
+        (pat (rest pattern)))
+      (if (null? pat)
+          (match-variable var input bindings)
+          (let ((pos (position (first pat) input start)))
+            (if (null? pos)
+                fail
+                (let ((b2 (pat-match pat (subseq input pos) bindings)))
+                    (if (eqv? b2 fail)
+                        (segment-match pattern input bindings (+ pos 1))
+                        (match-variable var (subseq input 0 pos) b2))))))))
+
+(define (subseq l offset n)
+    (when (and (>= offset 0) (<= n (sub1 length l)))
+        (take (drop l offset) n)))
+
+(define (segment-pattern? pattern)
+  (and (cons? pattern)
+       (starts-with? (car pattern) '?*)))
+
+(define (starts-with? list x)
+  (and (cons? list) (eqv? (car list) x)))
 
 (define (match-variable var input bindings)
     (let ((binding (get-binding var bindings)))
@@ -56,5 +83,6 @@
             bindings)))
 
 ;(pat-match '(?X + ?X) '((2 + 2) + (2 + 2)))
-(pat-match '(?P need . ?X) '(i need a long vacation))
+;(pat-match '(?P need . ?X) '(i need a long vacation))
+;(pat-match '((?* ?x) a b (?*?x)) '(1 2 a b a b 1 2 a b))
 ;(pat-match '((?* ?p) need '(?* ?)) '(Mr Hulot and I need a vacation))
